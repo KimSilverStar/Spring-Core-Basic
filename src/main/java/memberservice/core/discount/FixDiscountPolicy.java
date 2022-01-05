8234e3f62d94be5a886ff1fc285146dea427f28b
unknown <dmstjd365@kw.ac.kr>
2022-01-05 16:01:06 +0900

    할인 정책 인터페이스 DiscountPolicy, 정액 할인 정책 구현체 FixDiscountPolicy 추가

---
+++ src/main/java/memberservice/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,16 @@
+package memberservice.core.discount;
+
+import memberservice.core.member.Grade;
+import memberservice.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+	private int discountFixAmount = 1000;		// 1000원 할인
+
+	@Override
+	public int discount(Member member, int price) {
+		if (member.getGrade() == Grade.VIP)
+			return discountFixAmount;
+		else
+			return 0;
+	}
+}
