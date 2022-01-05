5159be9b2dae94506f12a3929ef209006698112e
unknown <dmstjd365@kw.ac.kr>
2022-01-05 15:53:36 +0900

    회원 저장소 인터페이스 MemberRepository, 메모리 회원 저장소 구현체 MemoryMemberRepository 추가

---
+++ src/main/java/memberservice/core/member/MemberRepository.java
@@ -0,0 +1,6 @@
+package memberservice.core.member;
+
+public interface MemberRepository {
+	void save(Member member);
+	Member findById(Long memberId);
+}
---
+++ src/main/java/memberservice/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package memberservice.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+	// ! 실무에서는 동시성 문제 고려하여 ConcurrentHashMap
+	public static Map<Long, Member> store = new HashMap<>();
+
+	@Override
+	public void save(Member member) {
+		store.put(member.getId(), member);
+	}
+
+	@Override
+	public Member findById(Long memberId) {
+		return store.get(memberId);
+	}
+}
