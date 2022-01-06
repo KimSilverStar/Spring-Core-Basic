1248911040e039c8f33a6fe3ba3d6d5c04f6f099
unknown <dmstjd365@kw.ac.kr>
2022-01-06 15:36:36 +0900

    AppConfig 리팩토링 - 역할 interface, 구현체 명확히 정리

--- src/main/java/memberservice/core/AppConfig.java
+++ src/main/java/memberservice/core/AppConfig.java
@@ -1,6 +1,8 @@
 package memberservice.core;
 
+import memberservice.core.discount.DiscountPolicy;
 import memberservice.core.discount.FixDiscountPolicy;
+import memberservice.core.member.MemberRepository;
 import memberservice.core.member.MemberService;
 import memberservice.core.member.MemberServiceImpl;
 import memberservice.core.member.MemoryMemberRepository;
@@ -8,14 +10,24 @@ import memberservice.core.order.OrderService;
 import memberservice.core.order.OrderServiceImpl;
 
 public class AppConfig {
+	// AppConfig 리팩토링 - 역할 interface 반환형 메소드에서 구현체 생성하여 반환
+	// => 역할과 구현이 명확히 보이도록 정리
+
 	public MemberService memberService() {
-		return new MemberServiceImpl(new MemoryMemberRepository());
+		return new MemberServiceImpl(memberRepository());	// 구현체
+	}
+
+	public MemberRepository memberRepository() {
+		return new MemoryMemberRepository();				// 구현체
 	}
 
 	public OrderService orderService() {
 		return new OrderServiceImpl(
-				new MemoryMemberRepository(),
-				new FixDiscountPolicy()
+				memberRepository(), discountPolicy()		// 구현체
 		);
+	}
+
+	public DiscountPolicy discountPolicy() {
+		return new FixDiscountPolicy();						// 구현체
 	}
 }
