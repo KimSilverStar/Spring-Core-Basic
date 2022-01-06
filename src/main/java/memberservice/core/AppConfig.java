5557e36f6b9e4e092dd8670b66fc0a225e835953
unknown <dmstjd365@kw.ac.kr>
2022-01-06 16:17:26 +0900

    AppConfig 수정 - 할인 정책 적용을 FixDiscountPolicy에서 RateDiscountPolicy로 변경

--- src/main/java/memberservice/core/AppConfig.java
+++ src/main/java/memberservice/core/AppConfig.java
@@ -2,6 +2,7 @@ package memberservice.core;
 
 import memberservice.core.discount.DiscountPolicy;
 import memberservice.core.discount.FixDiscountPolicy;
+import memberservice.core.discount.RateDiscountPolicy;
 import memberservice.core.member.MemberRepository;
 import memberservice.core.member.MemberService;
 import memberservice.core.member.MemberServiceImpl;
@@ -13,21 +14,27 @@ public class AppConfig {
 	// AppConfig 리팩토링 - 역할 interface 반환형 메소드에서 구현체 생성하여 반환
 	// => 역할과 구현이 명확히 보이도록 정리
 
+	// MemberService 역할
 	public MemberService memberService() {
 		return new MemberServiceImpl(memberRepository());	// 구현체
 	}
 
+	// MemberRepository 역할
 	public MemberRepository memberRepository() {
 		return new MemoryMemberRepository();				// 구현체
 	}
 
+	// OrderService 역할
 	public OrderService orderService() {
 		return new OrderServiceImpl(
 				memberRepository(), discountPolicy()		// 구현체
 		);
 	}
 
+	// DiscountPolicy 역할
 	public DiscountPolicy discountPolicy() {
-		return new FixDiscountPolicy();						// 구현체
+//		return new FixDiscountPolicy();						// 구현체
+		return new RateDiscountPolicy();
+		// 할인 정책 변경: FixDiscountPolicy -> RateDiscountPolicy
 	}
 }
