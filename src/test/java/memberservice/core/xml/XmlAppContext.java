7ab6ca0ee2820a61728501c0ce688bb54f97c1c2
unknown <dmstjd365@kw.ac.kr>
2022-01-08 16:38:14 +0900

    AppConfig 클래스의 설정 정보를 XML로 작성해봄

--- src/test/java/memberservice/core/xml/XmlAppContext.java
+++ src/test/java/memberservice/core/xml/XmlAppContext.java
@@ -1,2 +1,20 @@
-package memberservice.core.xml;public class XmlAppContext {
+package memberservice.core.xml;
+
+import memberservice.core.member.MemberService;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+	@Test
+	void xmlAppContext() {
+		ApplicationContext ac = new GenericXmlApplicationContext(
+				"appConfig.xml"
+		);
+		MemberService memberService = ac.getBean(
+				"memberService", MemberService.class
+		);
+		assertThat(memberService).isInstanceOf(MemberService.class);
+	}
 }
