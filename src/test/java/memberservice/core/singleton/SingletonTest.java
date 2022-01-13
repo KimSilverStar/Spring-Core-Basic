3f278be088892f3d2cf5d3d1905dc1e9d9a0cf57
unknown <dmstjd365@kw.ac.kr>
2022-01-13 17:05:58 +0900

    스프링 Singleton 컨테이너 테스트 케이스 작성

--- src/test/java/memberservice/core/singleton/SingletonTest.java
+++ src/test/java/memberservice/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import memberservice.core.member.MemberService;
 import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 	@Test
@@ -40,5 +42,30 @@ public class SingletonTest {
 
 		// singletonService1 == singletonService2
 		assertThat(singletonService1).isSameAs(singletonService2);
+	}
+
+	@Test
+	@DisplayName("스프링 컨테이너와 Singleton")
+	void springContainer() {
+		// 스프링 없는 순수 DI 컨테이너
+//		AppConfig appConfig = new AppConfig();
+
+		ApplicationContext ac =
+				new AnnotationConfigApplicationContext(AppConfig.class);
+
+		// Singleton 객체
+		MemberService memberService1 = ac.getBean(
+				"memberService", MemberService.class
+		);
+		MemberService memberService2 = ac.getBean(
+				"memberService", MemberService.class
+		);
+
+		// 참조값이 같음을 확인
+		System.out.println("memberService1 = " + memberService1);
+		System.out.println("memberService2 = " + memberService2);
+
+		// memberService1 == memberService2
+		assertThat(memberService1).isSameAs(memberService2);
 	}
 }
